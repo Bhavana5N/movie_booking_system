c2811aeb73b7ab5dbaad47a40fa9b8f8a9a94c87
Nahiyaan-Sheikh <81336214+Nahiyaan-Sheikh@users.noreply.github.com>
2021-09-22 20:22:48 -0400

    Fixed django

--- ebooking_system/ebooking/urls.py
+++ ebooking_system/ebooking/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', include('home.urls')),
 ]
