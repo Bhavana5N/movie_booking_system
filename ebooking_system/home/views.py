c59ae3cc77b291b20a64982d3e3d1081ef7a393e
Nahiyaan-Sheikh <81336214+Nahiyaan-Sheikh@users.noreply.github.com>
2021-09-22 21:15:20 -0400

    Added bootstrap theme and registration page

---
+++ ebooking_system/home/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+TEMPLATE_DIRS = (
+    'os.path.join(BASE_DIR, "templates"),'
+)
+
+def index(request):
+    return render(request, "index.html")
+
+
+
+def registration(request):
+    return render(request, 'registration.html')
