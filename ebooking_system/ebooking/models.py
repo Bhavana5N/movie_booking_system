e260f72f99c2c672b0c851dd79c327c7dddaf90f
cdrmacich@gmail.com <M@reep99>
2021-10-25 12:50:07 -0400

    Added customUser in registration update branch

--- ebooking_system/ebooking/models.py
+++ ebooking_system/ebooking/models.py
@@ -1,10 +1,13 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractUser
 from django.contrib.auth.forms import UserCreationForm
 from django.db import models
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    mobile = models.IntegerField()
+#class Profile(models.Model):
+#    user = models.OneToOneField(User, on_delete=models.CASCADE)
+#    mobile = models.IntegerField()
+
+class customUser(AbstractUser):
+    address = models.TextField()
 
 class UserRegisrationForm(models.Model):
     name = models.CharField()
