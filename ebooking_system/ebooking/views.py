3a95273abf2df940bde75a6dd69d26713a10f631
Bhavana5N <34437924+Bhavana5N@users.noreply.github.com>
2021-10-26 22:46:14 -0400

    reg

--- ebooking_system/ebooking/views.py
+++ ebooking_system/ebooking/views.py
@@ -33,8 +33,9 @@ def login_user(request):
 def admin(request):
     return render(request, 'admin.html')
 
+
 def regisconfirmation(request):
-    pass
+    return render(request, 'regisconfirmation.html')
 
 def forgot_password_view(request):
     if request.method == "POST":
@@ -135,10 +136,15 @@ def registration(request):
         if form.is_valid():
             print("valid")
             user = form.save(commit=False)
-            user.is_active = False
+            user.is_active = True
             user.username = user.email
             print("got here")
             user.save()
+            send_mail(
+                subject='Password Reset Link',
+                message="Your account is registered!\nPlease click on the following link to login:\n" + 'http://127.0.0.1:8080/login/',
+                from_email="n.bhavana.reddy5@outlook.com",
+                recipient_list=[user.email])
             return render(request, 'regisconfirmation.html')
         else:
             print("invalid")
@@ -146,7 +152,7 @@ def registration(request):
                 v = form.errors.get_json_data()[k][0]["message"]
                 messages.error(request, v)
                 print(v)
-            print(form.errors)
+            #print(form.errors)
             messages.info(request, f'Some detail made the form invalid. Try again!')
             return render(request, 'registration.html')
     else:
