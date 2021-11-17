8c7f6b420522f93eb6141f91b1dc76a3c2f9ebc0
Bhavana5N <34437924+Bhavana5N@users.noreply.github.com>
2021-11-16 23:14:33 -0500

    fixed category

--- ebooking_system/ebooking/views.py
+++ ebooking_system/ebooking/views.py
@@ -196,9 +196,14 @@ def index(request):
     new_movies = EbookingMovie.objects.filter(status="coming_soon")
     present_movies = EbookingMovie.objects.filter(status="airing")
     #print(movie[0].trailer_link)
-    return render(request, "index.html", {'new_movies': new_movies, 'present_movies': present_movies})
+    category_list = Category.objects.all()
+    print(category_list)
+    return render(request, "index.html", {'new_movies': new_movies, 'present_movies': present_movies,
+                                          'category_list': category_list})
 
 def base(request):
+    category_list = Category.objects.all()
+    print(category_list)
     if request.method == 'GET':
         movie_title = request.GET['movie_name']
         movie_category = 'ALL'
@@ -217,7 +222,7 @@ def base(request):
             "movie_count": count
         }
 
-        return render(request, 'searchResults.html', {'movie_list': movie_list})
+        return render(request, 'searchResults.html', {'movie_list': movie_list, 'category_list': category_list})
     
 def moviedetails(request):
     movie = EbookingMovie.objects.filter(movie_title="RRR")
@@ -235,7 +240,6 @@ def book_movie(request):
     for i in schedule_movie:
         stored_time = datetime.strftime(i.date_time, '%Y-%m-%dT%H:%M')
         if current_time < stored_time:
-
             my_date = datetime.strftime(i.date_time, "%Y-%m-%d")
             my_time = datetime.strftime(i.date_time, "%H:%M")
             if my_date in total_time_list:
@@ -287,13 +291,16 @@ def addpromotion(request):
 
 
 def addmovie(request):
+    category_list = Category.objects.all()
+    print(category_list)
     if request.method == 'POST':
         movie_details = request.POST
         try:
             print(request.POST)
             movie_object = EbookingMovie(movie_title=movie_details["title"],actors=movie_details["actors"],
                           status='coming_soon',  producer=movie_details["producer"],
-                          trailer_link=movie_details["trailerURL"], release_date=movie_details["releasedate"],
+                          trailer_link=movie_details["trailerURL"],
+            #, release_date=movie_details["releasedate"],
                           director=movie_details["director"], synopsis=movie_details["synopsis"],
                           category=movie_details["category"], ratings=movie_details["rating"],
                           age_category=movie_details["age_category"], runtime=movie_details["runtime"],
@@ -302,14 +309,14 @@ def addmovie(request):
             print(b)
             if b:
                 messages.info(request, f'A Movie with title already exists. Try again!')
-                return render(request, 'addmovie.html')
+                return render(request, 'addmovie.html', {'category_list': category_list})
             movie_object.save()
             messages.info(request, f'Movie is successfully Added')
         except Exception as e:
             print(e)
             messages.error(request, f'Movie is not Added')
-        return render(request, "addmovie.html")
-    return render(request, "addmovie.html")
+        return render(request, "addmovie.html", {'category_list': category_list})
+    return render(request, "addmovie.html", {'category_list': category_list})
 
 def schedule(request):
     all_movie_titles = EbookingMovie.objects.values_list('movie_title', flat=True)
