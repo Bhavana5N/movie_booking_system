af6d868089c542738cc28e356eab2a6ad37a9b32
cdrmacich@gmail.com <M@reep99>
2021-11-28 21:00:25 -0500

    Confirmation works!

--- ebooking_system/ebooking/urls.py
+++ ebooking_system/ebooking/urls.py
@@ -52,6 +52,7 @@ urlpatterns = [
     path('schedule', views.schedule, name='schedule'),
     #path('schedulemovie', views.schedulemovie, name='schedulemovie'),
     path('bookmovie/', views.book_movie, name='bookmovie'),
-    path('base', views.base, name='base')
+    path('base', views.base, name='base'),
+    path('orderhistory', views.orderHistory, name='orderhistory')
 ]
 
